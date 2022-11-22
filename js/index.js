271b08a618c73a232e004f84d258617bbe77f03e
jessica12492 <jessica12492@gmail.com>
2022-11-21 19:14:32 -0600

    <created js in index.js file>

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const today = new Date()
+const thisYear = today.getFullYear()
+console.log(today);
+console.log(thisYear);
+const footer = document.querySelector('footer');
+console.log(footer);
+const copyright = document.createElement('p');
+console.log(copyright)
+copyright.innerHTML = 'Jessica Baker 2022'
+document.body.appendChild(copyright)
+const skills = ["Management", "Communication", "Customer Service", "Coding"]
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul')
+
+for (let index = 0; index < skills.length; index++) {
+
+
+    const skill = document.createElement('li');
+
+    skill.innerText = skills[index];
+    skillsList.appendChild(skill)
+
+
+}
+
+console.log(skills)
+
+
+
+
+
+
